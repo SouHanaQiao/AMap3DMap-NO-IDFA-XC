ff9f73dab0518e47247494bb316fff8ccb64eaf0
葬花桥 <wangqinain@126.con>
2023-06-12 16:30:20 +0800

    9.7.0

--- Package.swift
+++ Package.swift
@@ -6,22 +6,25 @@ import PackageDescription
 let package = Package(
     name: "AMap3DMap-NO-IDFA-XC",
     platforms: [
-      .iOS(.v11),
+        .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AMap3DMap-NO-IDFA-XC",
-            targets: [ "AMap3DMap-NO-IDFA-XC"])
+            targets: [ "AMap3DMap-NO-IDFA-XC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SouHanaQiao/AMapFoundation-NO-IDFA-XC", from: "1.8.2")
+        .package(url: "https://github.com/SouHanaQiao/AMapFoundation-NO-IDFA-XC", from: "1.8.3")
     ],
     targets: [
-        .binaryTarget(name: "AMap3DMap-NO-IDFA", url: "https://github.com/SouHanaQiao/AMap3DMap-NO-IDFA-XC/releases/download/9.7.0/AMap3DMap-NO-IDFA.zip", checksum: "9532b5220ca264c48cbfd90b2a7ccf636f461aedb7efe9eba4e34a901a6a4061"),
+        .binaryTarget(name: "AMap3DMap-NO-IDFA", url: "https://github.com/SouHanaQiao/AMap3DMap-NO-IDFA-XC/releases/download/9.7.0/AMap3DMap-NO-IDFA.zip", checksum: "783321db38f491a2cf7bd91d178f0e40730caefaee2dea545429b9dd6c8a92dd"),
+//        .binaryTarget(name: "AMap3DMap-NO-IDFA", path: "./output/AMap3DMap-NO-IDFA/MAMapKit.xcframework"),
         .target(name: "AMap3DMap-NO-IDFA-XC", dependencies: [
             .product(name: "AMapFoundation-NO-IDFA-XC", package: "AMapFoundation-NO-IDFA-XC"),
             .target(name: "AMap3DMap-NO-IDFA")
+        ], exclude: [
+            "Resources"
         ], linkerSettings: [
             .linkedFramework("CFNetwork"),
             .linkedFramework("SystemConfiguration"),
